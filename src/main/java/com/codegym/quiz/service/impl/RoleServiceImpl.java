e8c41fe62812787336b0a1bbc1e6700f4c2740eb
quan200699 <maplestory2012vn@gmail.com>
2019-11-28 13:05:38 +0700

    register api

--- src/main/java/com/codegym/quiz/service/impl/RoleServiceImpl.java
+++ src/main/java/com/codegym/quiz/service/impl/RoleServiceImpl.java
@@ -4,7 +4,9 @@ import com.codegym.quiz.model.Role;
 import com.codegym.quiz.repository.RoleRepository;
 import com.codegym.quiz.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoleServiceImpl implements RoleService {
     @Autowired
     private RoleRepository roleRepository;
