589558195645c13f2ef6e2105b5075a2f94f1dd7
quan200699 <maplestory2012vn@gmail.com>
2019-12-12 11:42:00 +0700

    TypeofQuestion controller

---
+++ src/main/java/com/codegym/quiz/controller/TypeOfQuestionController.java
@@ -0,0 +1,30 @@
+package com.codegym.quiz.controller;
+
+import com.codegym.quiz.model.TypeOfQuestion;
+import com.codegym.quiz.service.TypeOfQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class TypeOfQuestionController {
+    @Autowired
+    private TypeOfQuestionService typeOfQuestionService;
+
+    @GetMapping("/typeOfQuestions")
+    public ResponseEntity<Iterable<TypeOfQuestion>> showAll() {
+        Iterable<TypeOfQuestion> typeOfQuestions = typeOfQuestionService.findAll();
+        return new ResponseEntity<>(typeOfQuestions, HttpStatus.OK);
+    }
+
+    @GetMapping("/typeOfQuestions/{id}")
+    public ResponseEntity<TypeOfQuestion> getTypeOfQuestion(@PathVariable Long id) {
+        Optional<TypeOfQuestion> typeOfQuestionOptional = typeOfQuestionService.findById(id);
+        return typeOfQuestionOptional.map(typeOfQuestion -> new ResponseEntity<>(typeOfQuestion, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
