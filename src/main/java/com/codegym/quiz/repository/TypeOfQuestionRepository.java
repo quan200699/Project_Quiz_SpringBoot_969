49584aba8399df61b28a56f8ff822644d9e6e5a2
quan200699 <maplestory2012vn@gmail.com>
2019-12-12 11:18:53 +0700

    TypeOfQuestion Repository

---
+++ src/main/java/com/codegym/quiz/repository/TypeOfQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.quiz.repository;
+
+import com.codegym.quiz.model.TypeOfQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeOfQuestionRepository extends JpaRepository<TypeOfQuestion, Long> {
+}
