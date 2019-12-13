02f0462d64ae23b3c8a74b758ac5226eca0ebd2a
quan200699 <maplestory2012vn@gmail.com>
2019-12-13 11:09:28 +0700

    get answer

--- src/main/java/com/codegym/quiz/controller/AnswerController.java
+++ src/main/java/com/codegym/quiz/controller/AnswerController.java
@@ -5,10 +5,15 @@ import com.codegym.quiz.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@CrossOrigin("*")
 public class AnswerController {
     @Autowired
     private AnswerService answerService;
@@ -17,5 +22,11 @@ public class AnswerController {
     public ResponseEntity<Iterable<Answer>> showAll() {
         Iterable<Answer> answers = answerService.findAll();
         return new ResponseEntity<>(answers, HttpStatus.OK);
+    }
+
+    @GetMapping("/answers/{id}")
+    public ResponseEntity<Answer> getAnswerDetail(@PathVariable Long id) {
+        Optional<Answer> answer = answerService.findById(id);
+        return answer.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
