7be08769777e40685a8c9657f8c51024ea09f46e
quan200699 <maplestory2012vn@gmail.com>
2019-12-14 13:53:08 +0700

    remove lib in question controller

--- src/main/java/com/codegym/quiz/controller/QuestionController.java
+++ src/main/java/com/codegym/quiz/controller/QuestionController.java
@@ -6,10 +6,8 @@ import com.codegym.quiz.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
