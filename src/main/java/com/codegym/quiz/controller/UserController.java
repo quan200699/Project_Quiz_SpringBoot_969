c6c4d086b12140e5aebcaa6e31d2917a7c20cb2a
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-01-04 18:37:54 +0700

    add id to JWT respone

--- src/main/java/com/codegym/quiz/controller/UserController.java
+++ src/main/java/com/codegym/quiz/controller/UserController.java
@@ -124,8 +124,8 @@ public class UserController {
 
         String jwt = jwtService.generateTokenLogin(authentication);
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), userDetails.getAuthorities()));
+        User currentUser = userService.findByUsername(user.getUsername());
+        return ResponseEntity.ok(new JwtResponse(jwt, currentUser.getId(), userDetails.getUsername(), userDetails.getAuthorities()));
     }
 
     @GetMapping("/users/{id}")
--- src/main/java/com/codegym/quiz/model/JwtResponse.java
+++ src/main/java/com/codegym/quiz/model/JwtResponse.java
@@ -5,15 +5,25 @@ import org.springframework.security.core.GrantedAuthority;
 import java.util.Collection;
 
 public class JwtResponse {
+    private Long id;
     private String token;
     private String type = "Bearer";
     private String username;
     private Collection<? extends GrantedAuthority> roles;
 
-    public JwtResponse(String accessToken, String username, Collection<? extends GrantedAuthority> roles) {
+    public JwtResponse(String accessToken, Long id, String username, Collection<? extends GrantedAuthority> roles) {
         this.token = accessToken;
         this.username = username;
         this.roles = roles;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getAccessToken() {
