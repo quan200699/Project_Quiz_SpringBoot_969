aa3a58407a4ef19b4ebe7cc10e7c4f9e00f8f8c7
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-02-03 11:26:41 +0700

    method deleteExam

--- src/main/java/com/codegym/quiz/controller/ExamController.java
+++ src/main/java/com/codegym/quiz/controller/ExamController.java
@@ -1,6 +1,7 @@
 package com.codegym.quiz.controller;
 
 import com.codegym.quiz.model.Exam;
+import com.codegym.quiz.model.Quiz;
 import com.codegym.quiz.service.ExamService;
 import com.codegym.quiz.service.UserService;
 import com.codegym.quiz.service.impl.EmailService;
@@ -46,5 +47,23 @@ public class ExamController {
         }
         examService.save(exam);
         return new ResponseEntity<>(exam,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/exams/{id}")
+    public ResponseEntity<Exam> updateExam(@RequestBody Exam exam, @PathVariable Long id) {
+        Optional<Exam> examOptional = examService.findById(id);
+        if (!examOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        exam.setId(examOptional.get().getId());
+        exam.setParticipants(examOptional.get().getParticipants());
+        examService.save(exam);
+        return new ResponseEntity<>(exam, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/exams/{id}")
+    public ResponseEntity<Exam> deleteExam(@PathVariable Long id) {
+        examService.remove(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
