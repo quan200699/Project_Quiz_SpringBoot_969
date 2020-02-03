642ead1e78f2d1f6bb646c32585667ae6871d6f3
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-02-03 09:57:07 +0700

    add method createExam

--- src/main/java/com/codegym/quiz/controller/ExamController.java
+++ src/main/java/com/codegym/quiz/controller/ExamController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -40,5 +37,14 @@ public class ExamController {
         Optional<Exam> examOptional = examService.findById(id);
         return examOptional.map(exam -> new ResponseEntity<>(exam, HttpStatus.OK)).
                 orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/exams")
+    public ResponseEntity<Exam> createExam(@RequestBody Exam exam){
+        if (exam.getMinutes().isBefore(exam.getStartedDate())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        examService.save(exam);
+        return new ResponseEntity<>(exam,HttpStatus.CREATED);
     }
 }
