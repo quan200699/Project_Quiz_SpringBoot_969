4e6ff0b32aa742f61e699841224153c99c463813
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-02-03 09:02:52 +0700

    create ExamController

---
+++ src/main/java/com/codegym/quiz/controller/ExamController.java
@@ -0,0 +1,9 @@
+package com.codegym.quiz.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class ExamController {
+}
