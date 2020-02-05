0f10f69f052882c0d50497b569154b1e132151d0
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-02-05 15:13:03 +0700

    add method in ResultRepository

--- src/main/java/com/codegym/quiz/repository/ResultRepository.java
+++ src/main/java/com/codegym/quiz/repository/ResultRepository.java
@@ -1,7 +1,11 @@
 package com.codegym.quiz.repository;
 
+import com.codegym.quiz.model.Exam;
 import com.codegym.quiz.model.Result;
+import com.codegym.quiz.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResultRepository extends JpaRepository<Result, Long> {
+    Iterable<Result> findAllByExam(Exam exam);
+    Result findAllByUser(User user);
 }
