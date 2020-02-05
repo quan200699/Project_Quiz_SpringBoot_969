1cf6458213aa4b4b48b399e0697d79b583a65b01
tunglam99 <lamnthnue.edu.vn@gmail.com>
2020-02-05 15:17:30 +0700

    create ResultServiceImpl

--- src/main/java/com/codegym/quiz/service/ResultService.java
+++ src/main/java/com/codegym/quiz/service/ResultService.java
@@ -1,6 +1,10 @@
 package com.codegym.quiz.service;
 
+import com.codegym.quiz.model.Exam;
 import com.codegym.quiz.model.Result;
+import com.codegym.quiz.model.User;
 
 public interface ResultService extends GeneralService<Result> {
+    Iterable<Result> findAllByExam(Exam exam);
+    Result findAllByUser(User user);
 }
---
+++ src/main/java/com/codegym/quiz/service/impl/ResultServiceImpl.java
@@ -0,0 +1,48 @@
+package com.codegym.quiz.service.impl;
+
+import com.codegym.quiz.model.Exam;
+import com.codegym.quiz.model.Result;
+import com.codegym.quiz.model.User;
+import com.codegym.quiz.repository.ResultRepository;
+import com.codegym.quiz.service.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ResultServiceImpl implements ResultService {
+
+    @Autowired
+    private ResultRepository resultRepository;
+
+    @Override
+    public Iterable<Result> findAllByExam(Exam exam) {
+        return resultRepository.findAllByExam(exam);
+    }
+
+    @Override
+    public Result findAllByUser(User user) {
+        return resultRepository.findAllByUser(user);
+    }
+
+    @Override
+    public Iterable<Result> findAll() {
+        return resultRepository.findAll();
+    }
+
+    @Override
+    public Optional<Result> findById(Long id) {
+        return resultRepository.findById(id);
+    }
+
+    @Override
+    public void save(Result model) {
+        resultRepository.save(model);
+    }
+
+    @Override
+    public void remove(Long id) {
+        resultRepository.deleteById(id);
+    }
+}
