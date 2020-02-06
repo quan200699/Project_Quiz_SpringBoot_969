d41a6cf2e83e39b9064cd342488df7eb29f54ce7
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-02-06 15:22:40 +0700

    add student class entity

---
+++ src/main/java/com/codegym/quiz/model/StudentClass.java
@@ -0,0 +1,21 @@
+package com.codegym.quiz.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class StudentClass implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
