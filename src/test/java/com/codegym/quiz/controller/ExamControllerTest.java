dc6300fd438811a0ba0cbff0037a47da1f2f0946
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-02-14 09:06:11 +0700

    find by id Exam  return status 200 with role tutor

--- src/test/java/com/codegym/quiz/controller/ExamControllerTest.java
+++ src/test/java/com/codegym/quiz/controller/ExamControllerTest.java
@@ -11,20 +11,22 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExamControllerTest {
@@ -38,14 +40,20 @@ public class ExamControllerTest {
 
     private List<Exam> exams;
 
+    private Exam exam1;
+
+    private Exam exam2;
+
     @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
-        Exam exam1 = examService.save(new Exam("No 1"));
-        Exam exam2 = examService.save(new Exam("No 2"));
+//        Exam exam1 = examService.save(new Exam("No 1"));
+//        Exam exam2 = examService.save(new Exam("No 2"));
+        exam1 = new Exam("No 1");
+        exam2 = new Exam("No 2");
         exams = new ArrayList<>();
         exams.add(exam1);
         exams.add(exam2);
@@ -85,5 +93,17 @@ public class ExamControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @WithMockUser(value = "tutor", roles = {"TUTOR"})
+    @DisplayName("find by id return status 200 with role tutor")
+    @Test
+    public void findById_whenGetExamsWithRoleTutor_thenReturnStatus200()
+            throws Exception {
+        given(examService.findById(1L)).willReturn(Optional.of(exam1));
+        mvc.perform(get("/exams/{id}",1L)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
     }
 }
